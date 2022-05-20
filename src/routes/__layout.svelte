286569562f40c7936028bca27032d21ee1cabf25
polarity <Charles.ran9@gmail.com>
2022-05-20 17:08:30 -0400

    Base Layout Setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,71 @@
+<script>
+	import '../app.scss';
+
+	import { Graphic } from '@smui/list';
+</script>
+
+<svelte:head>
+	<!-- Svelte MD -->
+	<link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
+	<!-- Material Icons -->
+	<link
+	rel="stylesheet"
+	href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	/>
+	<!-- Roboto -->
+	<link
+	rel="stylesheet"
+	href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+	/>
+	<!-- Roboto Mono -->
+	<link
+	rel="stylesheet"
+	href="https://fonts.googleapis.com/css?family=Roboto+Mono"
+	/>
+</svelte:head>
+
+<nav>
+	<div class = "logo-wrapper">
+		<img class = "logo" src = "vo_stroke.png">
+		Virtual Olympiad
+	</div>
+	<a href = "">
+		<span class="material-icons">
+			login
+		</span>
+	</a>
+</nav>
+<slot />
+
+<style lang="scss">
+	nav {
+		padding: .75rem 1rem;
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+
+		background-color: #5684C2;
+
+		.logo-wrapper {
+			margin-right: auto;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			color: #ffffff;
+
+			.logo {
+				height: 32px;
+				margin-right: .75rem;
+			}
+		}
+
+		a {
+			margin: 0 .75rem;
+
+			> span {
+				margin: 0;
+			}
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,95 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Textfield from '@smui/textfield';
+	import CharacterCounter from '@smui/textfield/character-counter';
+	import Paper, { Title, Subtitle, Content } from '@smui/paper';
+    import Drawer, { AppContent } from '@smui/drawer';
+	import List, { Item, Text, Graphic, Separator, Subheader } from '@smui/list';
+
+	// Player username
+	let username = '';
+
+	interface DrawerItem {
+		label: string;
+		icon?: string;
+	}
+
+    let drawerToggle = 0;
+    let drawerItems = [{label: 'Create', icon: 'add_circle'}, {label: 'Join', icon: 'travel_explore'}, /*{label: 'Custom', icon: 'star'},*/ {label: 'Contests', icon: 'verified'}];
+</script>
+
+<section>
+	<div class="drawer-container">
+		<Drawer>
+			<Content>
+                <div class="username-field">
+                    <Textfield variant="filled" bind:value={username} label="Username" input$maxlength={18}>
+                        <CharacterCounter slot="helper">0 / 18</CharacterCounter>
+                    </Textfield>
+                </div>
+				<List>
+                    {#each drawerItems as {label, icon}, i}
+					<Item on:click = {()=> {drawerToggle = i}}>
+						<Graphic class="material-icons">
+							{icon}
+						</Graphic>
+						<Text>{label}</Text>
+					</Item>
+                    {/each}
+				</List>
+
+				<!-- Footer Copyright -->
+				<footer class = "copyright">Math et al</footer>
+			</Content>
+		</Drawer>
+
+		<AppContent class="app-content">
+			<main class="main-content">
+				App content.
+				<br />
+			</main>
+		</AppContent>
+	</div>
+</section>
+
+<style lang="scss">
+    @import '../variables.scss';
+
+	section {
+		position: relative;
+		width: 100%;
+		height: 100%;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+        .drawer-container {
+            position: relative;
+            display: flex;
+            height: 100%;
+            width: 100%;
+            border: 1px solid $border-color;
+            overflow: hidden;
+            z-index: 0;
+
+			.username-field {
+				padding: 1rem;
+			}
+
+			.copyright {
+				position: absolute;
+				bottom: 0;
+
+				padding: .5rem;
+			}
+        }
+
+		.main-content {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: flex-start;
+		}
+	}
+</style>
