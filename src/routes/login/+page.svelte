a4452e187bd10d3bc580140114bbf35231b4e45d
polarr <Charles.Ran9@gmail.com>
2022-10-15 21:35:37 -0400

    feat: password reset logic on login route

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,7 +5,8 @@
 		PasswordInput,
 		Tile,
 		Button,
-		Link
+		Link,
+		InlineNotification
 	} from 'carbon-components-svelte';
 
 	import Login from 'carbon-icons-svelte/lib/Login.svelte';
@@ -50,20 +51,53 @@
 	const handleLogin = async () => {
 		try {
 			loading = true;
+			if (!loginPassword){
+				throw new Error('You must enter your password.');
+			}
 			const { error } = await supabase.auth.signIn({ email: loginEmail, password: loginPassword });
 			if (error) throw error;
 		} catch ({ error_description, message }) {
 			invalidCredentials = (error_description || message) as string;
-			console.log(invalidCredentials);
+			console.error(invalidCredentials);
 		} finally {
 			loading = false;
 		}
 
 		user.set(!!supabase.auth.user());
 	};
+	
+	let resetSent = false;
+	const handleReset = async () => {
+		if (loading) {
+			return;
+		}
+
+		try {
+			resetSent = false;
+			loading = true;
+			const { error } = await supabase.auth.api.resetPasswordForEmail(loginEmail, { redirectTo: 'https://voly.mathetal.org/resetpassword' });
+			if (error) throw error;
+			resetSent = true;
+		} catch ({ error_description, message }) {
+			invalidCredentials = (error_description || message) as string;
+			console.error(invalidCredentials);
+		} finally {
+			loading = false;
+		}
+	};
 </script>
 
 <section class="login-panel">
+	{#key resetSent}
+		{#if resetSent}
+			<InlineNotification
+				lowContrast
+				kind="success"
+				title="We've emailed you the password reset link"
+				subtitle="Wait up to a minute and check your spam folder"
+			/>
+		{/if}
+	{/key}
 	<Tile>
 		<h1 class="login-title" style="user-select: none;">Login</h1>
 		<h6 class="login-subtitle" style="user-select: none;">{subtitleDetail}</h6>
@@ -90,7 +124,7 @@
 		<div style="margin-top: 1rem; user-select: none;">
 			<Link href="/signup">Create an account</Link>
 			│
-			<Link href="">Forgot password?</Link>
+			<Link on:click={handleReset}>Forgot password?</Link>
 		</div>
 		<span style="display: flex; justify-content: flex-end; margin-top: 1rem;">
 			<Button on:click={handleLogin} type="submit" size="field" icon={Login} disabled={loading}
