54e15a715eaa80f37c489bca255198b1d9fdc0b7
polarr <Charles.Ran9@gmail.com>
2022-12-24 01:09:30 -0500

    feat: game timer, client vs saved answer verification, users sidebar

--- src/routes/live/Game.svelte
+++ src/routes/live/Game.svelte
@@ -26,7 +26,13 @@
 		Tag,
 		ButtonSet,
 		ProgressBar,
-		NumberInput
+		NumberInput,
+		InlineLoading,
+		StructuredList,
+		StructuredListHead,
+		StructuredListRow,
+		StructuredListCell,
+		StructuredListBody
 	} from 'carbon-components-svelte';
 	import Information from 'carbon-icons-svelte/lib/Information.svelte';
 	import AddFilled from 'carbon-icons-svelte/lib/AddFilled.svelte';
@@ -44,11 +50,18 @@
 	import { doc, getDoc } from 'firebase/firestore';
 	import { goto } from '$app/navigation';
 	import { browser } from '$app/environment';
-	import { afterUpdate, beforeUpdate } from 'svelte';
+	import { afterUpdate, beforeUpdate, onMount } from 'svelte';
+	import UsersMenu from './UsersMenu.svelte';
 
 	let users: any[] = [];
 	let problems: any[] = [];
+
+	let timeLimit = 0;
+	let startTime = window.performance.now();
+
 	let problemAnswers: any[] = [];
+	let savedAnswers: any[] = [];
+	let savingAnswers = false;
 
 	$: (problemAnswers || true) && updateAnswers();
 
@@ -124,13 +137,16 @@
 	};
 
 	const updateAnswers = async () => {
-		console.log(problemAnswers);
+		savingAnswers = true;
+
 		try {
 			await update(ref(rtdb, 'gameInfo/' + $room?.roomId + '/responses/' + $user.user.uid), {
 				answers: problemAnswers
 			});
 		} catch (error) {
 			console.error(error);
+		} finally {
+			savingAnswers = false;
 		}
 	};
 
@@ -148,38 +164,71 @@
 					})
 				)
 			).map((user) => user.value);
-			console.log(users);
 		} catch (error) {
 			console.error(error);
 		}
 	});
 
 	onValue(
-		ref(rtdb, 'gameInfo/' + $room?.roomId + '/responses/' + $user.uid + '/answers'),
+		ref(rtdb, 'gameInfo/' + $room?.roomId + '/responses/' + $user.user.uid + '/answers'),
 		async (snapshot) => {
 			if (!snapshot.exists()) {
 				return;
 			}
 
-			if (problems.length > 0) {
-				problemAnswers = snapshot.val();
-			}
+			savedAnswers = snapshot.val();
 		}
 	);
 
-	onValue(
-		ref(rtdb, 'gameInfo/' + $room?.roomId + '/problems'),
-		async (snapshot) => {
-			if (!snapshot.exists()) {
-				console.error('Error Fetching Problems: No problems in database');
-				return;
-			}
+	const saveStatus = (i) => {
+		if ((problemAnswers[i] ?? '') == (savedAnswers[i] ?? '')) {
+			return 'finished';
+		}
 
-			problems = snapshot.val();
-			problemAnswers = new Array(problems.length).fill('');
-		},
-		{ onlyOnce: true }
-	);
+		if (savingAnswers) {
+			return 'active';
+		}
+
+		return 'error';
+	};
+
+	const saveDescription = (i) => {
+		switch (saveStatus(i)) {
+			case 'finished':
+				return 'Saved';
+			case 'active':
+				return 'Saving';
+			case 'error':
+				return 'Unsaved';
+		}
+	};
+
+	let currentTime = Date.now();
+
+	const updateTimer = () => {
+		setTimeout(updateTimer, 1000);
+
+		currentTime = Date.now();
+	};
+
+	onMount(() => {
+		updateTimer();
+
+		onValue(
+			ref(rtdb, 'gameInfo/' + $room?.roomId + '/gameDetails'),
+			async (snapshot) => {
+				if (!snapshot.exists() || !snapshot.val().problems) {
+					console.error('Error Fetching Problems: No problems in database');
+					return;
+				}
+
+				({ startTime, timeLimit, problems } = snapshot.val());
+
+				problemAnswers = new Array(problems.length).fill('');
+			},
+			{ onlyOnce: true }
+		);
+	});
 
 	const exitRoom = async () => {
 		if (loading) return;
@@ -201,78 +250,202 @@
 	};
 </script>
 
-<section class="problem-panel">
+<section class="game-panel">
 	<Tile
-		style="width: 100%; height: 100%; display: flex; flex-direction: column; overflow-y: auto;"
-		class="live-panel-tile"
+		style="width: 100%; height: 100%; display: flex; align-items: center;"
+		class="game-panel-tile"
 	>
-		{#each problems as { problem, title, answerType }, i}
-			<div class="problem" id={'problem-' + i}>
-				<h4 class="problem-title">{title} <!--Problem {i + 1}--></h4>
-				<div class="problem-container">
-					{@html problem}
-				</div>
-
-				{#if answerType == 'amc'}
-					<RadioButtonGroup
-						legendText="Multiple Choice"
-						class="no-select"
-						bind:selected={problemAnswers[i]}
-					>
-						<RadioButton value="">
-							<span slot="labelText" style="font-size: 16px; font-weight: 400;"> - </span>
-						</RadioButton>
-						{#each ['a', 'b', 'c', 'd', 'e'] as choice, i}
-							<RadioButton value={choice}>
-								<span slot="labelText" style="font-size: 16px; font-weight: 400;">
-									{choice.toUpperCase()}
-								</span>
-							</RadioButton>
-						{/each}
-					</RadioButtonGroup>
-				{:else if answerType == 'aime'}
-					<NumberInput
-						allowEmpty
-						hideSteppers
-						light
-						size="sm"
-						label="Integer Answer"
-						min={0}
-						max={999}
-						bind:value={problemAnswers[i]}
-					/>
-				{/if}
+		<section class="details-panel">
+			<h4 class="no-select">{$room.roomInfo?.name}</h4>
+			<div class="room-invite">
+				<h6 class="no-select">Invite Code:</h6>
+				<CodeSnippet light code={$room.roomId} feedbackTimeout={1000} />
 			</div>
-		{/each}
+			<Tabs selected={1} class="flex-tabs">
+				<Tab label="Users" />
+				<Tab label="Submission" />
+				<svelte:fragment slot="content">
+					<TabContent style="overflow: auto; width: 100%; height: 100%; margin-bottom: 1rem;">
+						<UsersMenu {users} />
+					</TabContent>
+					<TabContent style="overflow: auto; width: 100%; height: 100%; margin-bottom: 1rem;">
+						<StructuredList condensed>
+							<StructuredListHead>
+								<StructuredListRow head>
+									<StructuredListCell head>Problem</StructuredListCell>
+									<StructuredListCell style="text-align: center;" head>Response</StructuredListCell>
+								</StructuredListRow>
+							</StructuredListHead>
+							<StructuredListBody>
+								{#each savedAnswers as answer, i}
+								<StructuredListRow>
+									<StructuredListCell noWrap>Problem {i + 1}</StructuredListCell>
+									<StructuredListCell style="text-align: center;">{answer?.toString().toUpperCase() || "-"}</StructuredListCell>
+								</StructuredListRow>
+								{/each}
+							</StructuredListBody>
+						</StructuredList>
+					</TabContent>
+				</svelte:fragment>
+			</Tabs>
+			<Button
+				on:click={exitRoom}
+				kind="secondary"
+				disabled={loading}
+				style="margin-top: auto;"
+				icon={Exit}
+			>
+				Exit Room
+			</Button>
+		</section>
+		<div class="exam-panel">
+			<article class="info-panel no-select">
+				<Tile light style="width: 100%; height: 100%; display: flex; align-items: center;">
+					<ProgressBar
+						value={timeLimit + startTime - currentTime}
+						max={timeLimit}
+						labelText="Time Remaining"
+						helperText={new Date(
+							Math.max(0, Math.min(timeLimit, timeLimit + startTime - currentTime))
+						)
+							.toISOString()
+							.slice(11, 19)}
+						style="width: 100%;"
+					/>
+				</Tile>
+			</article>
+			<section class="problem-panel">
+				{#each problems as { problem, title, answerType }, i}
+					<div class="problem no-select" id={'problem-' + i}>
+						<div style="display: flex; align-items: center;">
+							<h4 class="problem-title">
+								{title}
+								<!--Problem {i + 1}-->
+							</h4>
+
+							{#key savingAnswers ? problemAnswers[i] : null}
+								{#key savedAnswers[i]}
+									<InlineLoading
+										status={saveStatus(i)}
+										description={saveDescription(i) +
+											': ' +
+											(savedAnswers[i]?.toString().toUpperCase() || '-')}
+										style="padding: 0 1rem; width: fit-content"
+									/>
+								{/key}
+							{/key}
+						</div>
+						<div class="problem-container">
+							{@html problem}
+						</div>
+
+						{#if answerType == 'amc'}
+							<RadioButtonGroup legendText="Multiple Choice" bind:selected={problemAnswers[i]}>
+								<RadioButton value="">
+									<span slot="labelText" style="font-size: 16px; font-weight: 400;"> - </span>
+								</RadioButton>
+								{#each ['a', 'b', 'c', 'd', 'e'] as choice, i}
+									<RadioButton value={choice}>
+										<span slot="labelText" style="font-size: 16px; font-weight: 400;">
+											{choice.toUpperCase()}
+										</span>
+									</RadioButton>
+								{/each}
+							</RadioButtonGroup>
+						{:else if answerType == 'aime'}
+							<NumberInput
+								allowEmpty
+								hideSteppers
+								light
+								size="sm"
+								label="Integer Answer"
+								min={0}
+								max={999}
+								bind:value={problemAnswers[i]}
+							/>
+						{/if}
+					</div>
+				{/each}
+			</section>
+		</div>
 	</Tile>
 </section>
 
 <style lang="scss">
 	@import 'src/variables.scss';
 
-	.problem-panel {
-		width: min(100%, 1080px);
+	.game-panel {
+		width: min(100%, 1312px);
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		height: 100%;
 
-		.problem {
-			width: 100%;
-			padding: 1rem 0.5rem;
+		.details-panel {
+			display: flex;
+			flex: 1 0 0;
+			max-width: 480px;
+			height: 100%;
+			border-right: 1px solid #525252;
+
 			display: flex;
 			flex-direction: column;
-			border-bottom: 1px dotted #999;
+			align-items: center;
 
-			.problem-title {
-				font-weight: 500;
+			.room-invite {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				margin: 1rem 0;
 			}
 
-			.problem-container {
-				height: 100%;
-				overflow-x: auto;
-				padding: 1rem 0;
-				margin: 0 0 0.5rem 0;
+			.user-avatar {
+				object-fit: contain;
+				height: 4em;
+				border-radius: 50%;
+				outline: 1px solid #aaaaaa;
+			}
+		}
+
+		.exam-panel {
+			flex: 2.5 0 0;
+
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			height: 100%;
+
+			.info-panel {
+				width: 100%;
+
+				display: flex;
+				align-items: center;
+				border-bottom: 1px solid #525252;
+			}
+		}
+
+		.problem-panel {
+			flex: 2 0 0;
+			overflow-y: auto;
+			padding-left: 1rem;
+
+			.problem {
+				width: 100%;
+				padding: 1rem 0.5rem;
+				display: flex;
+				flex-direction: column;
+				border-bottom: 1px dotted #999;
+
+				.problem-title {
+					font-weight: 500;
+				}
+
+				.problem-container {
+					height: 100%;
+					overflow-x: auto;
+					padding: 1rem 0;
+					margin: 0 0 0.5rem 0;
+				}
 			}
 		}
 	}
