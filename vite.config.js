ce6b50a9c63f651996810bba65f51fc6bf67316a
polarr <Charles.Ran9@gmail.com>
2023-01-08 00:23:33 -0500

    chore: bump dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,24 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: '@use "src/variables.scss" as *;'
-            }
-        }
-    }
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@use "src/variables.scss" as *;'
+			}
+		}
+	},
+	optimizeDeps: {
+		exclude: [
+			'firebase',
+			'firebase/app',
+			'firebase/auth',
+			'firebase/firestore',
+			'firebase/analytics',
+			'firebase/database',
+			'firebase/storage'
+		]
+	}
 };
 
 export default config;
