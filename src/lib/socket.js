edf73c35d5e7558f6ba0d3fae42014214687b80c
polarity <Charles.Ran9@gmail.com>
2024-06-07 18:03:15 -0400

    fix: prune typescript to javascript maps pre-build

--- src/lib/socket.js
+++
@@ -1,5 +0,0 @@
-import ioClient from "socket.io-client";
-const ENDPOINT = import.meta.env.VITE_NODE_ENV == "development" ? "http://localhost:4000" : import.meta.env.VITE_SERVER_ENDPOINT;
-const io = ioClient(ENDPOINT);
-export const socket = io;
-//# sourceMappingURL=socket.js.map
